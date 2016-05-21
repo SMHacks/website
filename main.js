8c0147efe83c7722816d9ee9c1b3116fe88dc9fd
Ben Pang <benjinpang@gmail.com>
2016-05-21 14:29:30 -0700

    even more accurate

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 $(window).on('load scroll',function(){
 	$('.navbar-brand').css('color',$(this).scrollTop()>$(this).height()-30?'#ff7242':'#ffffff')
-	$(this).width()<1400&&$('.navbar').css('display',$(this).scrollTop()>$(this).height()-30?'none':'inline-block')
+	$(this).width()<1360&&$('.navbar').css('display',$(this).scrollTop()>$(this).height()-30?'none':'inline-block')
 })
