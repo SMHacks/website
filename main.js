cd798bbf4d9c4f6442599877bf6c9faea3047c61
Ben Pang <benjinpang@gmail.com>
2016-05-21 14:17:42 -0700

    FIX

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 $(window).on('load scroll',function(){
 	$('.navbar-brand').css('color',$(this).scrollTop()>$(this).height()-30?'#ff7242':'#ffffff')
-<<<<<<< HEAD
-=======
 	$(this).width()<992&&$('.navbar').css('display',$(this).scrollTop()>$(this).height()-30?'none':'inline-block')
->>>>>>> gh-pages
 })
