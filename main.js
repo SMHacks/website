c724bd82eee9af4d6964bde0822428506d450c23
Ben Pang <benjinpang@gmail.com>
2016-05-22 15:00:23 -0700

    Revert "fun fun fx"
    This reverts commit 12c4ef9cd524724539f33d3206575015df2a512c.

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-$('html,body').css('display','none').fadeIn(1000)
 $(window).on('load scroll',function(){
 	$('.navbar-brand').css('color',$(this).scrollTop()>$(this).height()-30?'#ff7242':'#ffffff')
 })
