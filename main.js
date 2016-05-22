aaa157572a0068bf1feae1032b511e9762cd8e4e
Ben Pang <benjinpang@gmail.com>
2016-05-22 15:07:39 -0700

    alright, let's do this over again.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+$('html,body').css('display','none').delay(500).fadeIn(1000)
 $(window).on('load scroll',function(){
 	$('.navbar-brand').css('color',$(this).scrollTop()>$(this).height()-30?'#ff7242':'#ffffff')
 	$(this).width()<1360&&$('.navbar').css('display',$(this).scrollTop()>$(this).height()-30?'none':'inline-block')
