7cfca0aeb487aa5cc7c02b02ba1c93964d20a486
Samuel Escapa <samuel.escapa@gmail.com>
2016-05-23 09:08:27 -0700

    Revert "glow!"

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ $('img').ready(function(){
 	$('html,body').delay(500).fadeIn(1000)
 })
 $(window).on('load scroll',function(){
-	$('.navbar-brand').css('color',$(this).scrollTop()>$(this).height()-30?'#ff7242':'#cccccc')
+	$('.navbar-brand').css('color',$(this).scrollTop()>$(this).height()-30?'#ff7242':'#333333')
 	$(this).width()<1360&&$('.navbar').css('display',$(this).scrollTop()>$(this).height()-30?'none':'inline-block')
 })
